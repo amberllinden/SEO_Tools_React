42f19351d6bcc173b035b120862b28f508e607ec
anastasiya.melnichenko <anastasiya.melnichenko@ramax.ru>
2020-08-12 12:52:41 +0300

    Main page

--- src/pages/main.js
+++ src/pages/main.js
@@ -4,11 +4,16 @@ import { TextComponent } from '../components/text';
 export class Main extends Component {
   render() {
     return (
-      <TextComponent
-        tag="h1"
-        content="Head"
-        styleClass="head-h1"
-      />
+      <>
+        <TextComponent
+          tag="h1"
+          content="Тут будет заголовок"
+        />
+        <TextComponent
+          tag="p"
+          content="Тут будет текст"
+        />
+      </>
     );
   }
 }
