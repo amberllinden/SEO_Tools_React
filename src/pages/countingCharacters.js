84a852556b0084510ae0c430a01606ab26c7955b
anastasiya.melnichenko <anastasiya.melnichenko@ramax.ru>
2020-10-19 17:36:25 +0300

    counting characters tool

---
+++ src/pages/countingCharacters.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Counting from '../components/Counting';
+
+const CountingCharacters = () => {
+    return (
+        <Counting />
+    )
+}
+
+export default CountingCharacters;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Main from '../pages/main';
 import ArraysComparison from '../pages/arraysComparison';
 import WordProcessor from '../pages/wordProcessor';
+import CountingCharacters from '../pages/countingCharacters';
 
 const Routes = {
   '/': () => <Main />,
   '/arrays_comparison': () => <ArraysComparison />,
   '/word_processor': () => <WordProcessor />,
+  '/counting_characters': () => <CountingCharacters />,
 };
 export default Routes;
