c19161f349e6359fe00671a6f5f74bd3b97a8883
anastasiya.melnichenko <anastasiya.melnichenko@ramax.ru>
2020-10-19 12:58:58 +0300

    word processortool

---
+++ src/pages/wordProcessor.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Processor from '../components/Processor';
+
+const WordProcessor = () => {
+    return (
+        <Processor />
+    )
+}
+
+export default WordProcessor;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Main from '../pages/main';
 import ArraysComparison from '../pages/arraysComparison';
+import WordProcessor from '../pages/wordProcessor';
 
 const Routes = {
   '/': () => <Main />,
   '/arrays_comparison': () => <ArraysComparison />,
+  '/word_processor': () => <WordProcessor />,
 };
 export default Routes;
